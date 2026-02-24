b520195a9c793c2fadc5c67edaad86e1c75023a2
mndev2019 <muskanvision2019@gmail.com>
2026-02-24 15:39:41 +0530

    `Added Google Analytics integration to React app`

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useLocation } from 'react-router-dom'
 import './App.css'
 import WebLayout from './Layout/WebLayout'
 
 import Homenew from './Pages/Homenew'
+import { useEffect } from 'react';
 
 
 
+// --- 1. Create a Analytics Wrapper ---
+// This component listens for URL changes and sends them to Google
+function AnalyticsTracker() {
+  const location = useLocation();
 
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null; // This component doesn't render anything
+}
 function App() {
   const ThemeRoute = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<WebLayout />}>
+        {/* <Route path="/" element={<WebLayout />}> */}
+         <Route path="/" element={
+          <>
+            <AnalyticsTracker /> {/* Tracks every route change */}
+            <WebLayout />
+          </>
+        }>
 
           <Route index element={<Homenew />} />
         
